df8695bbe360b63a6090e5d71ab18edce4dfd588
rohan <rohanverma031@gmail.com>
2025-07-13 21:53:00 +0530

    feat: added mobile nav bar

--- src/components/Nav/NavLinks.tsx
+++ src/components/Nav/NavLinks.tsx
@@ -2,14 +2,57 @@ import { navLinks } from "@/utils/nav";
 import styles from "./nav.module.css";
 import { useMediaQuery } from "usehooks-ts";
 import { Link } from "@tanstack/react-router";
+import { useState } from "react";
+
+const NavSm = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={styles["links-sm"]}>
+      <img
+        src="/menu.svg"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      />
+
+      <div
+        className={styles["links"]}
+        {...(open && {
+          "data-open": true,
+        })}
+      >
+        <img
+          src="/x.svg"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        />
+
+        {navLinks.links.map((item) => {
+          return (
+            <Link
+              to={item.value}
+              key={item.value}
+              activeProps={{
+                "data-active": true,
+              }}
+              onClick={() => setOpen(false)}
+            >
+              {item.key}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 const NavLinks = () => {
   const matches = useMediaQuery("(min-width: 64em)");
 
   return !matches ? (
-    <div>
-      <img src="/menu.svg" />
-    </div>
+    <NavSm />
   ) : (
     <div className={styles["links-lg"]}>
       {navLinks.links.map((item) => {
