beb93a941286c107b93535bfb57596587a7dd745
srdeveloper-ssci <srdeveloper@srisaiconsultancyinfosoft.in>
2026-03-09 17:23:34 +0530

    deploy fixed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/hero/hero";
 import HomeAbout from "@/components/Home/About/About";
-import Showcase from "@/components/Home/Showcase/Showcase";
+
 import Reviews from "@/components/Home/review/Reviews";
 import { WebpageValue } from "@/utils/pages";
 import { createFileRoute } from "@tanstack/react-router";
@@ -20,7 +20,6 @@ export const Route = createFileRoute("/")({
 });
 
 function App() {
-  const matches = useMediaQuery("(min-width: 45rem)");
 
   return (
     <div className="home-page">
